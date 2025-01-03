12347aed2e7be9f82a0b5d2581fba1aa4909e5dd
Alfredo Mariño <alfredoalejandro05@gmail.com>
2025-01-03 01:20:54 +0100

    utf8 changed

--- scripts/mybooksdb-backup.sql
+++ scripts/mybooksdb-backup.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
 --
 -- Host: localhost    Database: mybooksdb
 -- ------------------------------------------------------
--- Server version	8.0.29
+-- Server version	8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,10 +24,10 @@ DROP TABLE IF EXISTS `author`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `author` (
   `author_id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`author_id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,16 +67,16 @@ DROP TABLE IF EXISTS `book`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `book` (
   `book_id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(256) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
-  `subtitle` varchar(256) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci DEFAULT NULL,
-  `description` text CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci,
+  `title` varchar(256) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `subtitle` varchar(256) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_spanish_ci,
   `publication_date` date DEFAULT NULL,
-  `image` varchar(512) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
-  `isbn10` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci DEFAULT NULL,
-  `isbn13` varchar(13) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci DEFAULT NULL,
-  `google_id` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
+  `image` varchar(512) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `isbn10` varchar(10) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `isbn13` varchar(13) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `google_id` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`book_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,7 +126,7 @@ CREATE TABLE `categorize` (
   KEY `categoryid_idx` (`category_id`),
   CONSTRAINT `bookid_fk` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
   CONSTRAINT `categoryid_fk` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -193,9 +193,9 @@ DROP TABLE IF EXISTS `category`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `category` (
   `category_id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `name` varchar(256) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -241,9 +241,9 @@ DROP TABLE IF EXISTS `country`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `country` (
   `country_id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
+  `name` varchar(256) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`country_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=247 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=247 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -486,7 +486,7 @@ CREATE TABLE `library` (
   `end_country_id` int NOT NULL,
   `end_region_id` int NOT NULL,
   `rating` double NOT NULL,
-  `note` text CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci,
+  `note` text CHARACTER SET utf8 COLLATE utf8_spanish_ci,
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`book_id`,`person_id`),
   KEY `idperson` (`person_id`),
@@ -501,7 +501,7 @@ CREATE TABLE `library` (
   CONSTRAINT `library_ibfk_4` FOREIGN KEY (`end_country_id`) REFERENCES `country` (`country_id`),
   CONSTRAINT `library_ibfk_5` FOREIGN KEY (`initial_region_id`) REFERENCES `region` (`region_id`),
   CONSTRAINT `library_ibfk_6` FOREIGN KEY (`end_region_id`) REFERENCES `region` (`region_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -547,15 +547,15 @@ DROP TABLE IF EXISTS `person`;
 CREATE TABLE `person` (
   `person_id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
-  `last_name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `last_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `birthdate` date NOT NULL,
   `native_country_id` int NOT NULL,
   `native_region_id` int NOT NULL,
   `current_country_id` int NOT NULL,
   `current_region_id` int NOT NULL,
-  `about_me` text CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
-  `photo` varchar(256) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
+  `about_me` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `photo` varchar(256) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`person_id`),
   UNIQUE KEY `iduser_2` (`user_id`),
   KEY `iduser` (`user_id`),
@@ -568,7 +568,7 @@ CREATE TABLE `person` (
   CONSTRAINT `person_ibfk_3` FOREIGN KEY (`current_country_id`) REFERENCES `country` (`country_id`),
   CONSTRAINT `person_ibfk_4` FOREIGN KEY (`native_region_id`) REFERENCES `region` (`region_id`),
   CONSTRAINT `person_ibfk_5` FOREIGN KEY (`current_region_id`) REFERENCES `region` (`region_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -591,11 +591,11 @@ DROP TABLE IF EXISTS `region`;
 CREATE TABLE `region` (
   `region_id` int NOT NULL AUTO_INCREMENT,
   `country_id` int NOT NULL,
-  `name` varchar(256) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
+  `name` varchar(256) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`region_id`),
   KEY `country` (`country_id`),
   CONSTRAINT `region_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2204 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2204 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -2615,13 +2615,13 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
-  `password` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci NOT NULL,
+  `username` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(30) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `user_state` tinyint NOT NULL DEFAULT '1' COMMENT '1: Activo | 2: Inactivo',
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `nameunico` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -2648,7 +2648,7 @@ CREATE TABLE `written` (
   KEY `authorid_idx` (`author_id`),
   CONSTRAINT `authorid` FOREIGN KEY (`author_id`) REFERENCES `author` (`author_id`),
   CONSTRAINT `bookid` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
